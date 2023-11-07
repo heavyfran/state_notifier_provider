aa263bc896b60c0436eb45c4c64ce085c2383347
heavyfran <heavyfran@gmail.com>
2023-11-08 00:21:36 +0900

    StateNotifierProvider: todosProvider

---
+++ lib/pages/todos_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/todo_model.dart';
+
+class TodosNotifier extends StateNotifier<List<Todo>> {
+  TodosNotifier() : super([]);
+
+  void addTodo(String desc) {
+    state = [...state, Todo.add(desc: desc)];
+  }
+
+  void toggleTodo(String id) {
+    state = [
+      for (final todo in state)
+        if (todo.id == id) todo.copyWith(completed: !todo.completed) else todo
+    ];
+  }
+
+  void removeTodo(String id) {
+    state = [
+      for (final todo in state)
+        if (todo.id != id) todo
+    ];
+  }
+}
+
+final todosProvider = StateNotifierProvider<TodosNotifier, List<Todo>>((ref) {
+  return TodosNotifier();
+});
